84281e08c80a383125f5a116bc8a1591e4b83049
ashinsabu <ashin.sabu3@gmail.com>
2022-07-03 23:00:14 +0530

    player vs player mode implemented for single round

--- script.js
+++ script.js
@@ -35,9 +35,24 @@ const gameController = (() => {
     const player2 = Player ('O');
     let round = 1;
     let gameOver = false;
+    const messagearea = document.querySelector('.message');
+
     const playRound = (fieldIndex) => {
         gameBoard.setField(fieldIndex,getCurrentPlayer());
+        messagearea.innerHTML= (getCurrentPlayer()==player1 ? "Player O's turn":"Player X's turn");
         round++;
+        if(checkGameOver() !== " "){
+            if(checkGameOver() === "Draw"){
+                messagearea.innerHTML = "It's a draw.";
+                messagearea.style.color = "orange";
+                gameOver=true;
+            }
+            else{
+                messagearea.innerHTML = "Player " + checkGameOver() + " wins!";
+                messagearea.style.color = "blue";
+                gameOver=true;
+            }
+        }
     }
     const getCurrentPlayer = () => {
         return round % 2 === 1 ? player1: player2;
@@ -45,17 +60,55 @@ const gameController = (() => {
     const getGameOver = () => gameOver;
     const getRound = () => round;
 
-    const checkCols = () =>{
+    const checkCols = () => {
         if(gameBoard.getField(0) == gameBoard.getField(3) && gameBoard.getField(3) == gameBoard.getField(6) && gameBoard.getField(0)!=undefined)
-            return true;
+            return gameBoard.getField(0);
         if(gameBoard.getField(1) == gameBoard.getField(4) && gameBoard.getField(4) == gameBoard.getField(7) && gameBoard.getField(1)!=undefined)
-            return true;
-        if(gameBoard.getField(2) == gameBoard.getField(5) && gameBoard.getField(5) == gameBoard.getField(8) && gameBoard.getField(3)!=undefined)
-            return true;
+            return gameBoard.getField(1);
+        if(gameBoard.getField(2) == gameBoard.getField(5) && gameBoard.getField(5) == gameBoard.getField(8) && gameBoard.getField(2)!=undefined)
+            return gameBoard.getField(2);
+        return (" ");
+    }
+
+    const checkRows = () => {
+        if(gameBoard.getField(0) == gameBoard.getField(1) && gameBoard.getField(1) == gameBoard.getField(2) && gameBoard.getField(0)!=undefined)
+            return gameBoard.getField(0);
+        if(gameBoard.getField(3) == gameBoard.getField(4) && gameBoard.getField(4) == gameBoard.getField(5) && gameBoard.getField(5)!=undefined)
+            return gameBoard.getField(3);
+        if(gameBoard.getField(6) == gameBoard.getField(7) && gameBoard.getField(7) == gameBoard.getField(8) && gameBoard.getField(8)!=undefined)
+            return gameBoard.getField(6);
+        return (" ");
+    }
+    /* 
+       0 1 2
+       3 4 5
+       6 7 8
+    */
+    const checkDiags = () => {
+        if(gameBoard.getField(0) == gameBoard.getField(4) && gameBoard.getField(4) == gameBoard.getField(8) && gameBoard.getField(8)!=undefined)
+            return gameBoard.getField(0);
+           
+        if(gameBoard.getField(2) == gameBoard.getField(4) && gameBoard.getField(4) == gameBoard.getField(6) && gameBoard.getField(6)!=undefined)
+            return gameBoard.getField(2);
+        return (" ");
     }
 
     const checkGameOver = () => {
-
+        if(checkRows() !== " "){
+            console.log(checkRows() + "winbyrow");
+            return checkRows();
+        }
+        else if(checkCols() !== " "){
+            console.log(checkCols() + "winbycol");
+            return checkCols();
+        }
+        else if(checkDiags() !== " "){
+            console.log(checkDiags() + "winbydiag");
+            return checkDiags();
+        }
+        else if(round==10)
+            return ("Draw");
+        else return (" ");
     }
     return( {playRound,getCurrentPlayer,getGameOver,getRound} );
 
@@ -71,6 +124,8 @@ const displayController = (() => {
             gamemodeselector.classList.add('invisible');
             const boardContainer = document.querySelector('.board-container');
             boardContainer.classList.remove('invisible');
+            const messagearea = document.querySelector('.message');
+            messagearea.innerHTML= "Player X's turn";
 
         })
     })
